6848240efc414768c801251b1aff580efd5c0f28
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-07-17 16:50:50 +0200

    som tests

--- includes/minishell.h
+++ includes/minishell.h
@@ -4,7 +4,12 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
-#include "libutils.h"
+# include "libutils.h"
+
+# include "exec.h"
+# include "parsing.h"
 
 #endif
