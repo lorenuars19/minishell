1c35c16849d4a2810210ca50b84429d90c8dd84a
adrien <adrien51296@hotmail.com>
2021-07-28 20:12:25 +0200

    now get the value for one variable from envp

--- src/variable_expansion.c
+++ src/variable_expansion.c
@@ -69,13 +69,32 @@ t_bool	should_token_be_expanded(t_token *token)
 	return (FALSE);
 }
 
+char	*get_value_from_envp(char *name, char **envp)
+{
+	int		i;
+	size_t	length;
+
+	i = 0;
+	length = str_len(name);
+	while (envp[i])
+	{
+		if (str_cmp_n(envp[i], name, length) == 0 && envp[i][length] == '=')
+			return (str_dupli(envp[i]));
+		i++;
+	}
+	return (NULL);
+}
+
 char	*expand_one_variable(t_token *token, char **envp)
 {
 	(void)token;
 	(void)envp;
 
 	char *name = get_variable_name(token);
-	return (name);
+	if (!name)
+		return (NULL);
+	char *value = get_value_from_envp(name, envp);
+	return (value);
 }
 
 void	expand_variables(char **envp, t_token *tokens)
