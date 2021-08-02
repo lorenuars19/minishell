b057723a46a0b0bbf1672f087f55bfe3812055e4
adrien <adrien51296@hotmail.com>
2021-08-03 01:08:33 +0200

    added random_test.sh script

---
+++ random_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+	printf "Usage : %s [head -n]\n" $0
+	exit 0
+fi
+
+cat /dev/urandom | head -n $1 | tr -cd '[:print:]' > random.txt
+
+printf "\nexit\n" >> random.txt
+
+./minishell < random.txt
