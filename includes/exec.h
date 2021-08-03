603b5cef129792e58864399b9c9a8f17f184f2c9
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-03 14:41:56 +0200

    piping context

--- includes/exec.h
+++ includes/exec.h
@@ -1,7 +1,15 @@
 #ifndef EXEC_H
 # define EXEC_H
 
-int	exec_nodes(t_node *nodes, void *ctx);
+#define FORKED_CHILD 0
+
+typedef struct s_pipe_context
+{
+	int	fd[2];
+	int	close_fd;
+}	t_ctx;
+
+int	execution(t_node *nodes);
 
 
 #endif
