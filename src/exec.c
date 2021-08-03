e160838665f7c1485f5cccf858e34beb85dd9415
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-03 14:52:23 +0200

    fix err

--- src/exec.c
+++ src/exec.c
@@ -14,16 +14,16 @@ int exec_nodes(t_node *node, t_ctx * ctx)
 	if (node->type == COMMAND_NODE)
 	{
 		if (exec_command(node))
-			return (ERR);
+			return (1);
 	}
 	else if (node->type == PIPE_NODE)
 	{
 		if (exec_piped(node))
-			return (ERR);
+			return (1);
 	}
 	else
 	{
-		return (ERR);
+		return (1);
 	}
 	return (0);
 }
@@ -31,7 +31,7 @@ int exec_nodes(t_node *node, t_ctx * ctx)
 int exec_command(t_node *node)
 {
 	if (node->type != COMMAND_NODE)
-		return (ERR);
+		return (1);
 
 	if (fork == FORKED_CHILD)
 }
