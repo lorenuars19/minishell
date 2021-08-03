baafa4fa89a8ea9efbe20df830104c8660ce64d6
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-03 20:44:00 +0200

    add protos

--- includes/exec.h
+++ includes/exec.h
@@ -13,7 +13,10 @@ int	execution(t_node *node, char *envp[]);
 
 int exec_nodes(t_node *node, t_ctx * ctx, char *envp[]);
 
-int exec_command(t_node *node);
+int exec_command(t_node *node, char *envp[]);
+int wait_for_child(pid_t cpid);
+int check_for_builtins(t_node *node, char *envp[]);
+int	exec_builtin(t_node *node, char *envp[]);
 
 int exec_piped(t_node * node, t_ctx *ctx, char *envp[]);
 
