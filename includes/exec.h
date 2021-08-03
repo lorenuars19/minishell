f3373bb8b9354c1308fd61ac7f2abf212072b041
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-03 21:05:24 +0200

    add envp to builtins + unset

--- includes/exec.h
+++ includes/exec.h
@@ -2,6 +2,17 @@
 # define EXEC_H
 
 #define FORKED_CHILD 0
+typedef enum e_builtin_commands
+{
+	BUILTIN_ECHO,
+	BUILTIN_CD,
+	BUILTIN_PWD,
+	BUILTIN_EXPORT,
+	BUILTIN_ENV,
+	BUILTIN_UNSET,
+	BUILTIN_EXIT,
+	BUILTIN_END
+}	t_builtins;
 
 typedef struct s_pipe_context
 {
@@ -16,7 +27,16 @@ int exec_nodes(t_node *node, t_ctx * ctx, char *envp[]);
 int exec_command(t_node *node, char *envp[]);
 int wait_for_child(pid_t cpid);
 int check_for_builtins(t_node *node, char *envp[]);
-int	exec_builtin(t_node *node, char *envp[]);
+int	exec_builtin(t_node *node, char *envp[], int index);
+
+int builtin_echo(char *argv[], char *envp[]);
+int builtin_cd(char *argv[], char *envp[]);
+int builtin_pwd(char *argv[], char *envp[]);
+int builtin_export(char *argv[], char *envp[]);
+int builtin_env(char *argv[], char *envp[]);
+int builtin_exit(char *argv[], char *envp[]);
+
+
 
 int exec_piped(t_node * node, t_ctx *ctx, char *envp[]);
 
--- src/builtins.c
+++ src/builtins.c
@@ -1,37 +1,51 @@
 #include "minishell.h"
 
-int builtin_echo(char *argv[])
+int builtin_echo(char *argv[], char *envp[])
 {
+	(void)envp;
+	int i;
+
+	i = 1;
+
+	while (argv && argv[i])
+	{
+		put_str_fd(1, argv[i]);
+		i++;
+	}
+	return (0);
+}
+
+int builtin_cd(char *argv[], char *envp[])
+{
+	(void)envp;
 	(void)argv;
 	return (0);
 }
 
-int builtin_cd(char *argv[])
+int builtin_pwd(char *argv[], char *envp[])
 {
+	(void)envp;
 	(void)argv;
 	return (0);
 }
 
-int builtin_pwd(char *argv[])
+int builtin_export(char *argv[], char *envp[])
 {
+	(void)envp;
 	(void)argv;
 	return (0);
 }
 
-int builtin_export(char *argv[])
+int builtin_env(char *argv[], char *envp[])
 {
+	(void)envp;
 	(void)argv;
 	return (0);
 }
 
-int builtin_env(char *argv[])
+int builtin_exit(char *argv[], char *envp[])
 {
-	(void)argv;
-	return (0);
-}
-
-int builtin_exit(char *argv[])
-{
+	(void)envp;
 	(void)argv;
 	return (0);
 }
