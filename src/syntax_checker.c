a6465c0e53f756f5e7948f31369e11fcdb048efa
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-03 14:08:31 +0200

    fix syntax_checker

--- src/syntax_checker.c
+++ src/syntax_checker.c
@@ -2,19 +2,17 @@
 
 t_bool	is_there_unclosed_quotes(char *line)
 {
-	int	i;
 	t_bool	in_quotes;
 	char	delimiter;
 
-	i = 0;
 	in_quotes = FALSE;
 	while (*line)
 	{
 		if (in_quotes == TRUE && *line == delimiter)
-			in_quotes == FALSE;
+			in_quotes = FALSE;
 		else if (in_quotes == FALSE && (*line == '\'' || *line == '\"'))
 		{
-			in_quotes == TRUE;
+			in_quotes = TRUE;
 			delimiter = *line;
 		}
 		line++;
