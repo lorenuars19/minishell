48980f63549521346848bcd62faf9fdea285ee76
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-04 16:54:25 +0200

    add code for finding external binaries

--- src/exec.c
+++ src/exec.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "minishell.h"
 
-int execution(t_node *node, char *envp[])
+int	execution(t_node *node, char *envp[])
 {
 	t_ctx ctx;
 
@@ -11,7 +11,7 @@ int execution(t_node *node, char *envp[])
 	return (0);
 }
 
-int exec_nodes(t_node *node, t_ctx *ctx, char *envp[])
+int	exec_nodes(t_node *node, t_ctx *ctx, char *envp[])
 {
 	if (node->type == COMMAND_NODE)
 	{
@@ -30,7 +30,126 @@ int exec_nodes(t_node *node, t_ctx *ctx, char *envp[])
 	return (0);
 }
 
-int find_and_execute(t_node *node)
+int	check_for_builtins(t_node *node, char *envp[])
+{
+	int i;
+	static char *builtins[] = {
+		"echo",
+		"cd",
+		"pwd",
+		"export",
+		"unset",
+		"env",
+		"exit",
+		NULL};
+	i = 0;
+	while (node->args && node->args[0] && builtins[i] && str_cmp(node->args[0], builtins[i]))
+		i++;
+	if (exec_builtin(node, envp, i))
+		return (1);
+	return (0);
+}
+
+int	exec_builtin(t_node *node, char *envp[], int index)
+{
+	static int (*builtins[])(char *argv[], char *envp[]) = {
+		builtin_echo,
+		builtin_cd,
+		builtin_pwd,
+		builtin_export,
+		builtin_unset,
+		builtin_env,
+		builtin_exit};
+	if (index >= 0 && index < BUILTIN_END)
+	{
+		if (builtins[index](node->args, envp))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	exec_command(t_node *node, char *envp[])
+{
+	int status;
+	pid_t cpid;
+
+	if (check_for_builtins(node, envp))
+		return (1);
+	cpid = fork();
+	if (cpid < 0)
+		return (error_sys_put(errno));
+	else if (cpid == FORKED_CHILD)
+		return (exec_binary(node, envp));
+	else
+	{
+		//TODO parent stuff
+		status = wait_for_child(cpid);
+		if (status)
+		{
+			return (error_printf(status, "child exit code : %d\n", status));
+		}
+		//TODO close pipe ? or other sutff maybe IDK
+	}
+	return (0);
+}
+
+int	sub_wait_for_child(int wstatus)
+{
+	if (WIFEXITED(wstatus))
+		return (WEXITSTATUS(wstatus));
+	else if (WIFSIGNALED(wstatus))
+		return (WTERMSIG(wstatus));
+	else if (WIFSTOPPED(wstatus))
+		return (WSTOPSIG(wstatus));
+	return (wstatus);
+}
+
+int	wait_for_child(pid_t cpid)
+{
+	int ret;
+	int wstatus;
+	pid_t w;
+
+	w = waitpid(cpid, &wstatus, WUNTRACED);
+	if (w == -1)
+		return (-1);
+	ret = sub_wait_for_child(wstatus);
+	if (ret)
+		return (ret);
+	while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus))
+	{
+		w = waitpid(cpid, &wstatus, WUNTRACED);
+		ret = sub_wait_for_child(wstatus);
+		if (ret)
+			return (ret);
+	}
+	return (ret);
+}
+
+int	exec_piped(t_node *node, t_ctx *ctx, char *envp[])
+{
+	if (node->type != PIPE_NODE)
+		return (1);
+
+	(void)ctx;
+	(void)envp;
+
+	return (0);
+}
+
+int	exec_binary(t_node *node, char *envp[])
+{
+	char	*path;
+	int		status;
+
+	status = 0;
+
+	return (status);
+}
+
+int find_path(t_node *node)
 {
 	char *path;
 	char **tab;
@@ -54,133 +173,6 @@ int find_and_execute(t_node *node)
 	// while (
 	// 	exec (join(tab[i], ))
 	// )
-
-	return (0);
-}
-
-int check_for_builtins(t_node *node, char *envp[])
-{
-	int			i;
-	static char	*builtins[] = {
-		"echo",
-		"cd",
-		"pwd",
-		"export",
-		"unset",
-		"env",
-		"exit",
-		NULL
-	};
-	i = 0;
-	while (node->args && node->args[0] && builtins[i]
-		&& str_cmp(node->args[0], builtins[i]))
-		i++;
-	if (exec_builtin(node, envp, i))
-		return (1);
-	return (0);
-}
-
-int exec_builtin(t_node *node, char *envp[], int index)
-{
-	static int (*builtins[])(char *argv[], char *envp[]) = {
-		builtin_echo,
-		builtin_cd,
-		builtin_pwd,
-		builtin_export,
-		builtin_unset,
-		builtin_env,
-		builtin_exit
-	};
-	if (index >= 0 && index < BUILTIN_END)
-	{
-		if (builtins[index](node->args, envp))
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-
-int exec_command(t_node *node, char *envp[])
-{
-	int status;
-	pid_t cpid;
-
-
-	if (check_for_builtins(node, envp))
-	{
-		return (1);
-	}
-
-	cpid = fork();
-	if (cpid < 0)
-	{
-		return (error_sys_put(errno));
-	}
-	else if (cpid == FORKED_CHILD)
-	{
-
-		return (0);
-	}
-	else
-	{
-		//TODO parent stuff
-		status = wait_for_child(cpid);
-		if (status)
-		{
-			return (error_printf(status, "child exit code : %d\n", status));
-		}
-		//TODO close pipe ? or other sutff maybe IDK
-	}
-	return (0);
-}
-
-int	sub_wait_for_child(int wstatus)
-{
-	if (WIFEXITED(wstatus))
-		{
-			return (WEXITSTATUS(wstatus));
-		}
-		else if (WIFSIGNALED(wstatus))
-		{
-			return (WTERMSIG(wstatus));
-		}
-		else if (WIFSTOPPED(wstatus))
-		{
-			return (WSTOPSIG(wstatus));
-		}
-	return (wstatus);
-}
-
-int wait_for_child(pid_t cpid)
-{
-	int	ret;
-	int	wstatus;
-	pid_t	w;
-
-	w = waitpid(cpid, &wstatus, WUNTRACED);
-	if (w == -1)
-		return (-1);
-	ret = sub_wait_for_child(wstatus);
-	if (ret)
-		return (ret);
-	while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus))
-	{
-		w = waitpid(cpid, &wstatus, WUNTRACED);
-		ret = sub_wait_for_child(wstatus);
-		if (ret)
-			return (ret);
-	}
-	return (ret);
-}
-
-int exec_piped(t_node *node, t_ctx *ctx, char *envp[])
-{
-	if (node->type != PIPE_NODE)
-		return (1);
-
-	(void)ctx;
-	(void)envp;
 
 	return (0);
 }
