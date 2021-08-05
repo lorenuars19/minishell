bfda6c224be195d320a3f6b5ed31d61049c6e507
adrien <adrien51296@hotmail.com>
2021-08-06 00:48:26 +0200

    added unset builtin

--- src/builtins.c
+++ src/builtins.c
@@ -2,25 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-// int exec_command(t_node *node, char *envp[])
-// {
-// 	if (node->type != COMMAND_NODE || node->args == NULL)
-// 		return (0);
-// 	if (node->args[0] && strcmp(node->args[0], "echo") == 0)
-// 	{
-// 		builtin_echo(node->args, envp);
-// 	}
-// 	else if (node->args[0] && strcmp(node->args[0], "env") == 0)
-// 	{
-// 		builtin_env(node->args, envp);
-// 	}
-// 	else if (node->args[0] && strcmp(node->args[0], "pwd") == 0)
-// 	{
-// 		builtin_pwd(node->args, envp);
-// 	}
-// 	return (0);
-// }
-
 static int	get_envp_length(char **envp)
 {
 	int	i;
@@ -123,10 +104,76 @@ int builtin_export(char *argv[], char *envp[])
 	return (0);
 }
 
+static t_bool	is_a_valid_variable_name(char *name)
+{
+	int	i;
+
+	if (!name)
+		return (FALSE);
+	if (name[0] != '_' && !is_alpha(name[0]))
+		return (FALSE);
+	i = 0;
+	while (name[i])
+	{
+		if (!is_alnum(name[i]) && name[i] != '_')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+static void	shift_envp_up(char **envp, int i)
+{
+	while (envp[i])
+	{
+		envp[i] = envp[i + 1];
+		i++;
+	}
+}
+
+static void	delete_variable_from_envp(char *name, char **envp)
+{
+	int	i;
+	int	length;
+
+	i = 0;
+	length = str_len(name);
+	while (envp && envp[i])
+	{
+		if (str_cmp_n(name, envp[i], length) == 0 && envp[i][length] == '=')
+		{
+			free(envp[i]);
+			shift_envp_up(envp, i);
+			return ;
+		}
+		i++;
+	}
+}
+
 int builtin_unset(char *argv[], char *envp[])
 {
-	(void)envp;
-	(void)argv;
+	int	i;
+	t_bool	has_met_an_error;
+
+	if (!argv[1])
+		return (0);
+	i = 1;
+	has_met_an_error = FALSE;
+	while (argv && argv[i])
+	{
+		if (!is_a_valid_variable_name(argv[i]))
+		{
+			put_str_fd(STDERR_FILENO, "minishell: unset: '");
+			put_str_fd(STDERR_FILENO, argv[i]);
+			put_str_fd(STDERR_FILENO, "': not a valid identifier\n");
+			has_met_an_error = TRUE;
+		}
+		else
+			delete_variable_from_envp(argv[i], envp);
+		i++;
+	}
+	if (has_met_an_error)
+		return (1);
 	return (0);
 }
 
