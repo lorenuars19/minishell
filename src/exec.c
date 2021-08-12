44e60786d3a75427fd007950a6a7369b51437504
adrien <adrien51296@hotmail.com>
2021-08-12 23:31:52 +0200

    added some debugging info

--- src/exec.c
+++ src/exec.c
@@ -13,9 +13,11 @@ int	exec(t_node *node, char *envp[])
 	children = exec_node(node, &ctx, envp);
 	printf("number of children spawned: %d\n", children);
 	i = 0;
+	(void)i;
 	while (i < children)
 	{
 		wait(NULL);
+		printf("%d th command done\n", i);
 		i++;
 	}
 	return (0);
@@ -53,15 +55,24 @@ int	exec_command(t_node *node, t_context *ctx, char *envp[])
 	}
 	else if (cpid == FORKED_CHILD)
 	{
-		dup2(ctx->fd[STDIN_FILENO], STDIN_FILENO);
-		dup2(ctx->fd[STDOUT_FILENO], STDOUT_FILENO);
+		if (ctx->fd[STDIN_FILENO] != STDIN_FILENO)
+			dup2(ctx->fd[STDIN_FILENO], STDIN_FILENO);
+		if (ctx->fd[STDOUT_FILENO] != STDOUT_FILENO)
+			dup2(ctx->fd[STDOUT_FILENO], STDOUT_FILENO);
 		if (ctx->fd_close != -1)
 			close(ctx->fd_close);
 		execvp(node->args[0], node->args);
 		//TODO print on stderror
 		printf("error: %s\n", strerror(errno));
 	}
+	// waitpid(cpid, NULL, 0);
+	// printf("command %s terminated\n", node->args[0]);
 	return (1);
+}
+
+void print_context(t_context *ctx, char *name)
+{
+	printf("context %s: in:%d, out:%d, close:%d\n", name, ctx->fd[0], ctx->fd[1], ctx->fd_close);
 }
 
 int exec_pipe(t_node *node, t_context *ctx, char *envp[])
@@ -73,20 +84,25 @@ int exec_pipe(t_node *node, t_context *ctx, char *envp[])
 	t_context	rhs_ctx;
 	int children;
 
+	printf("context: in:%d, out:%d, close:%d\n", ctx->fd[0], ctx->fd[1], ctx->fd_close);
+	printf("command: left:%s, right: %s\n", node->left->args[0], (node->right->type == COMMAND_NODE ? node->right->args[0] : "PIPE"));
 	if (pipe(pipe_fd) == -1)
 	{
 		//TODO print on stderror
 		printf("minishell: pipe: %s\n", strerror(errno));
 		return (-1);
 	}
+	printf("pipe: fd0: %d, fd1: %d\n", pipe_fd[0], pipe_fd[1]);
 	lhs_ctx = *ctx;
 	lhs_ctx.fd[STDOUT_FILENO] = pipe_fd[STDOUT_FILENO];
 	lhs_ctx.fd_close = pipe_fd[STDIN_FILENO];
+	print_context(&lhs_ctx, "lhs");
 	lhs = node->left;
 	children = exec_node(lhs, &lhs_ctx, envp);
 	rhs_ctx = *ctx;
 	rhs_ctx.fd[STDIN_FILENO] = pipe_fd[STDIN_FILENO];
 	rhs_ctx.fd_close = pipe_fd[STDOUT_FILENO];
+	print_context(&rhs_ctx, "rhs");
 	rhs = node->right;
 	children += exec_node(rhs, &rhs_ctx, envp);
 
