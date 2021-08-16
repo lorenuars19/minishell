4cbb89bd0e77fce07c71f596adb5bf03c9f1baff
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-08-16 20:23:08 +0200

    commit new builtins from `origin/parsing` + del test.c

--- src/exec/builtins.c
+++ src/exec/builtins.c
@@ -1,18 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   builtins.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/09 18:49:48 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/08/09 18:49:50 by lorenuar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "minishell.h"
 #include <unistd.h>
 #include <stdlib.h>
+
+static int	get_envp_length(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	return (i);
+}
+
+char	**make_envp_copy(char **envp)
+{
+	char	**copy;
+	int		length;
+	int		i;
+
+	if (!envp)
+		return (NULL);
+	length = get_envp_length(envp);
+	copy = ft_calloc(length + 1, sizeof(char *));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		copy[i] = str_dupli(envp[i]);
+		i++;
+	}
+	return (copy);
+}
+
+void	free_envp(char **envp)
+{
+	int	i;
+
+	if (!envp)
+		return ;
+	i = 0;
+	while (envp[i])
+	{
+		free(envp[i]);
+		i++;
+	}
+	free(envp);
+}
 
 int builtin_echo(char *argv[], char *envp[])
 {
@@ -20,12 +55,15 @@ int builtin_echo(char *argv[], char *envp[])
 	t_bool	should_print_trailing_nl;
 
 	(void)envp;
-	i = 1;
-	should_print_trailing_nl = TRUE;
 	if (*argv && argv[1] && str_cmp(argv[1], "-n") == 0)
 	{
 		should_print_trailing_nl = FALSE;
 		i = 2;
+	}
+	else
+	{
+		should_print_trailing_nl = TRUE;
+		i = 1;
 	}
 	while (argv[i])
 	{
@@ -41,18 +79,29 @@ int builtin_echo(char *argv[], char *envp[])
 
 int builtin_cd(char *argv[], char *envp[])
 {
-	(void)envp;
+	char	*path;
 
-	char *path;
-
-	if (argv && !argv[1])
-		path = getenv("HOME");
-	else if (argv && argv[1])
-		path = argv[1];
-	if (chdir(path))
+	if (!argv[1])
+		path = get_value_from_envp("HOME", envp);
+	else if (argv[2])
 	{
-		return (error_sys_put("chdir"));
+		put_str_fd(STDERR_FILENO, "minishell: cd: too many arguments\n");
+		return (1);
 	}
+	else
+		path = str_dupli(argv[1]);
+	if (!path)
+		return (1);
+	if (chdir(path) == -1)
+	{
+		put_str_fd(STDERR_FILENO, "minishell: cd: ");
+		put_str_fd(STDERR_FILENO, path);
+		put_str_fd(STDERR_FILENO, ": ");
+		put_str_fd_nl(STDERR_FILENO, strerror(errno));
+		free(path);
+		return (1);
+	}
+	free(path);
 	return (0);
 }
 
@@ -70,27 +119,228 @@ int builtin_pwd(char *argv[], char *envp[])
 	return (0);
 }
 
-int builtin_export(char *argv[], char *envp[])
+t_bool	variable_already_exits_in_envp(char **envp, char *name)
 {
-	(void)envp;
-	(void)argv;
-	return (0);
+	int	i;
+	int	length;
+
+	if (!envp)
+		return (FALSE);
+	i = 0;
+	length = str_len(name);
+	while (envp[i])
+	{
+		if (str_cmp_n(name, envp[i], length) == 0 && envp[i][length] == '=')
+			return (TRUE);
+		i++;
+	}
+	return (FALSE);
+}
+
+static t_bool	is_a_valid_variable_name(char *name)
+{
+	int	i;
+
+	if (!name)
+		return (FALSE);
+	if (name[0] != '_' && !is_alpha(name[0]))
+		return (FALSE);
+	i = 0;
+	while (name[i])
+	{
+		if (!is_alnum(name[i]) && name[i] != '_')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+static void	shift_envp_up(char **envp, int i)
+{
+	while (envp[i])
+	{
+		envp[i] = envp[i + 1];
+		i++;
+	}
+}
+
+static void	delete_variable_from_envp(char *name, char **envp)
+{
+	int	i;
+	int	length;
+
+	i = 0;
+	length = str_len(name);
+	while (envp && envp[i])
+	{
+		if (str_cmp_n(name, envp[i], length) == 0 && envp[i][length] == '=')
+		{
+			free(envp[i]);
+			shift_envp_up(envp, i);
+			return ;
+		}
+		i++;
+	}
 }
 
 int builtin_unset(char *argv[], char *envp[])
 {
-	(void)envp;
-	(void)argv;
+	int	i;
+	t_bool	has_met_an_error;
+
+	if (!argv[1])
+		return (0);
+	i = 1;
+	has_met_an_error = FALSE;
+	while (argv && argv[i])
+	{
+		if (!is_a_valid_variable_name(argv[i]))
+		{
+			put_str_fd(STDERR_FILENO, "minishell: unset: '");
+			put_str_fd(STDERR_FILENO, argv[i]);
+			put_str_fd(STDERR_FILENO, "': not a valid identifier\n");
+			has_met_an_error = TRUE;
+		}
+		else
+			delete_variable_from_envp(argv[i], envp);
+		i++;
+	}
+	if (has_met_an_error)
+		return (1);
+	return (0);
+}
+
+static void	sort_envp(char **envp)
+{
+	t_bool		has_a_swap_occured;
+	char		*tmp;
+	int			i;
+
+	if (!envp || !envp[0])
+		return ;
+	has_a_swap_occured = TRUE;
+	while (has_a_swap_occured)
+	{
+		has_a_swap_occured = FALSE;
+		i = 0;
+		while (envp[i + 1])
+		{
+			if (strcmp(envp[i], envp[i + 1]) > 0)
+			{
+				has_a_swap_occured = TRUE;
+				tmp = envp[i];
+				envp[i] = envp[i + 1];
+				envp[i + 1] = tmp;
+			}
+			i++;
+		}
+	}
+}
+
+static void	print_one_exported_variable(char *variable)
+{
+	int	i;
+
+	if (!variable)
+		return ;
+	if (!str_cmp_n("_=", variable, 2))
+		return ;
+	printf("declare -x ");
+	i = 0;
+	while (variable[i] && variable[i] != '=')
+	{
+		printf("%c", variable[i]);
+		i++;
+	}
+	if (variable[i] == '\0')
+		return ;
+	printf("=\"");
+	printf("%s", variable + i + 1);
+	printf("\"\n");
+}
+
+static int	print_sorted_envp(char **envp)
+{
+	char	**dup;
+	int		i;
+
+	dup = make_envp_copy(envp);
+	if (!dup)
+		return (1);
+	sort_envp(dup);
+	i = 0;
+	while (dup[i])
+	{
+		print_one_exported_variable(dup[i]);
+		i++;
+	}
+	free_envp(dup);
+	return (0);
+}
+
+static t_bool is_a_valid_exported_name(char *name)
+{
+	int i;
+
+	if (!name)
+		return (FALSE);
+	if (name[0] != '_' && !is_alpha(name[0]))
+		return (FALSE);
+	i = 0;
+	while (name[i] && name[i] != '=')
+	{
+		if (!is_alnum(name[i]) && name[i] != '_')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+static void	print_export_name_error(char *name)
+{
+	put_str_fd(STDERR_FILENO, "minishell: export: '");
+	put_str_fd(STDERR_FILENO, name);
+	put_str_fd(STDERR_FILENO, "': not a valid identifier\n");
+}
+
+int builtin_export(char *argv[], char *envp[])
+{
+	int		i;
+	t_bool	has_met_an_error;
+
+	if (!argv[1])
+		return (print_sorted_envp(envp));
+	has_met_an_error = FALSE;
+	i = 1;
+	while (argv[i])
+	{
+		if (!is_a_valid_exported_name(argv[i]))
+		{
+			print_export_name_error(argv[i]);
+			has_met_an_error = TRUE;
+			i++;
+			continue ;
+		}
+		// if (var_already_exits_in_envp(argv[i]))
+		// 		modify_var_in_envp(envp, argv[i]);
+		// else
+		//		insert_new_value_in_envp(envp, argv[i]) //need to change envp to global for this
+		i++;
+	}
+	if (has_met_an_error)
+		return (1);
 	return (0);
 }
 
 int builtin_env(char *argv[], char *envp[])
 {
-	int i;
+	int	i;
 
 	(void)argv;
+	if (!envp)
+		return (1);
 	i = 0;
-	while (envp && envp[i])
+	while (envp[i])
 	{
 		printf("%s\n", envp[i]);
 		i++;
@@ -98,9 +348,9 @@ int builtin_env(char *argv[], char *envp[])
 	return (0);
 }
 
-int	builtin_dummy(char *argv[], char *envp[])
+int builtin_exit(char *argv[], char *envp[])
 {
-	(void)argv;
 	(void)envp;
+	(void)argv;
 	return (0);
 }
